04d846beb5b6de61fb9fe981df5235bc5a52a5f2
andrewens <andrewens4@gmail.com>
2024-09-03 09:32:09 -0600

    Create SoccerDuels client when a player joins the game

--- src/starter-player-scripts/Main.client.lua
+++ src/starter-player-scripts/Main.client.lua
@@ -1,1 +1,8 @@
-print("Hello world, from client!")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local SoccerDuels = require(ReplicatedStorage.SoccerDuels)
+
+local LocalPlayer = Players.LocalPlayer
+
+SoccerDuels.newClient(LocalPlayer)
