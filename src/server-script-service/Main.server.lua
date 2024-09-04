ccaa2153c54a44598d2c6ed67307e3e1ccc09f12
andrewens <andrewens4@gmail.com>
2024-09-04 11:39:33 -0600

    Destroy testing infrastructure if we're not in TestingMode

--- src/server-script-service/Main.server.lua
+++ src/server-script-service/Main.server.lua
@@ -1,14 +1,20 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local TestsFolder = ReplicatedStorage.Tests
+local TestEZModule = ReplicatedStorage.TestEZ
+local MockInstanceModule = ReplicatedStorage.MockInstance
 
-local MockInstance = require(ReplicatedStorage.MockInstance)
 local SoccerDuels = require(ReplicatedStorage.SoccerDuels)
-local TestEZ = require(ReplicatedStorage.TestEZ)
+local MockInstance = require(MockInstanceModule)
+local TestEZ = require(TestEZModule)
 
 SoccerDuels.initialize()
 
 if SoccerDuels.getConstant("TestingMode") then
-    MockInstance.initialize()
-    TestEZ.TestBootstrap:run({ TestsFolder })
+	MockInstance.initialize()
+	TestEZ.TestBootstrap:run({ TestsFolder })
+else
+	MockInstanceModule:Destroy()
+	TestEZModule:Destroy()
+	TestsFolder:Destroy()
 end
